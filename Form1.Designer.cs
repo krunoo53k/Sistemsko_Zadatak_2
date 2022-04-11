7c541be054a57c777edf5134f7a79c1ae400c596
Kruno Petrik <petrik.kpetrik@gmail.com>
2022-04-11 21:58:27 +0200

    Finished :)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.notepadButton = new System.Windows.Forms.Button();
+            this.paintButton = new System.Windows.Forms.Button();
+            this.cmdButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // notepadButton
+            // 
+            this.notepadButton.Location = new System.Drawing.Point(12, 73);
+            this.notepadButton.Name = "notepadButton";
+            this.notepadButton.Size = new System.Drawing.Size(75, 58);
+            this.notepadButton.TabIndex = 0;
+            this.notepadButton.Text = "notepad";
+            this.notepadButton.UseVisualStyleBackColor = true;
+            this.notepadButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // paintButton
+            // 
+            this.paintButton.Location = new System.Drawing.Point(126, 73);
+            this.paintButton.Name = "paintButton";
+            this.paintButton.Size = new System.Drawing.Size(75, 58);
+            this.paintButton.TabIndex = 1;
+            this.paintButton.Text = "paint";
+            this.paintButton.UseVisualStyleBackColor = true;
+            this.paintButton.Click += new System.EventHandler(this.paintButton_Click);
+            // 
+            // cmdButton
+            // 
+            this.cmdButton.Location = new System.Drawing.Point(235, 73);
+            this.cmdButton.Name = "cmdButton";
+            this.cmdButton.Size = new System.Drawing.Size(75, 58);
+            this.cmdButton.TabIndex = 2;
+            this.cmdButton.Text = "CMD";
+            this.cmdButton.UseVisualStyleBackColor = true;
+            this.cmdButton.Click += new System.EventHandler(this.cmdButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(328, 175);
+            this.Controls.Add(this.cmdButton);
+            this.Controls.Add(this.paintButton);
+            this.Controls.Add(this.notepadButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button notepadButton;
+        private System.Windows.Forms.Button paintButton;
+        private System.Windows.Forms.Button cmdButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Sistemsko_Zad_2
 {
@@ -15,6 +17,37 @@ namespace Sistemsko_Zad_2
         public Form1()
         {
             InitializeComponent();
+            Button.CheckForIllegalCrossThreadCalls = false;
+        }
+        private void openApp(string appName, Button button)
+        {
+            button.Enabled = false;
+            Process process = Process.Start(appName);
+            process.WaitForExit();
+            button.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(() => openApp("notepad", notepadButton));
+            thread.Start();
+        }
+
+        private void paintButton_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(() => openApp("mspaint.exe", paintButton));
+            thread.Start();
+        }
+
+        private void cmdButton_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(() => openApp("cmd", cmdButton));
+            thread.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
